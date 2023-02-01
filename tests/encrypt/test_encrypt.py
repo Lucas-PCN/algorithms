24e200304f659708f2ad5893cb3738971342363d
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-02-01 19:26:10 -0300

    Feat: add req 02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(31, 4)
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("message", "not_a_key")
+    assert encrypt_message("teste", 4) == "e_tset"
+    assert encrypt_message("teste", 1) == "t_etse"
+    assert encrypt_message("teste", 9) == "etset"
