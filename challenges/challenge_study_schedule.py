68524427bf4a030a5f0e3a43b73d3317af21334f
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-02-01 19:10:12 -0300

    Feat: add req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        students_studying = 0
+
+        for start, end in permanence_period:
+            if start <= target_time <= end:
+                students_studying += 1
+
+        return students_studying
+
+    except TypeError:
+        return None
